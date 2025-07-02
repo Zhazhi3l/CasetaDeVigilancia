6cb3c1cc23ac73444292b8824ce74d85a938c755
HazZav <kevlinlal@gmail.com>
2025-07-01 22:41:07 -0600

    Agregar archivos de proyecto.

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CasetaDeVigilancia
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSesionGuardia = new System.Windows.Forms.Button();
+            this.btnHistorial = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            label1.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.ForeColor = System.Drawing.Color.DarkGreen;
+            label1.Location = new System.Drawing.Point(32, 160);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(316, 126);
+            label1.TabIndex = 5;
+            label1.Text = "Bienvenido a:\r\nPuerta segura";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.panel1.Controls.Add(this.btnSesionGuardia);
+            this.panel1.Controls.Add(this.btnHistorial);
+            this.panel1.Controls.Add(this.btnRegistro);
+            this.panel1.Controls.Add(this.btnAcceder);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 450);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnSesionGuardia
+            // 
+            this.btnSesionGuardia.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSesionGuardia.FlatAppearance.BorderColor = System.Drawing.Color.MintCream;
+            this.btnSesionGuardia.FlatAppearance.BorderSize = 0;
+            this.btnSesionGuardia.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnSesionGuardia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSesionGuardia.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSesionGuardia.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSesionGuardia.Image = global::CasetaDeVigilancia.Properties.Resources.iniciar_sesion;
+            this.btnSesionGuardia.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSesionGuardia.Location = new System.Drawing.Point(0, 286);
+            this.btnSesionGuardia.Name = "btnSesionGuardia";
+            this.btnSesionGuardia.Size = new System.Drawing.Size(267, 50);
+            this.btnSesionGuardia.TabIndex = 10;
+            this.btnSesionGuardia.Text = "Sesión Guardia";
+            this.btnSesionGuardia.UseVisualStyleBackColor = true;
+            // 
+            // btnHistorial
+            // 
+            this.btnHistorial.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHistorial.FlatAppearance.BorderColor = System.Drawing.Color.MintCream;
+            this.btnHistorial.FlatAppearance.BorderSize = 0;
+            this.btnHistorial.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnHistorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHistorial.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHistorial.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHistorial.Image = global::CasetaDeVigilancia.Properties.Resources.historial_de_pedidos;
+            this.btnHistorial.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHistorial.Location = new System.Drawing.Point(0, 221);
+            this.btnHistorial.Name = "btnHistorial";
+            this.btnHistorial.Size = new System.Drawing.Size(267, 65);
+            this.btnHistorial.TabIndex = 9;
+            this.btnHistorial.Text = "Historial de\r\nAccesos";
+            this.btnHistorial.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRegistro.FlatAppearance.BorderColor = System.Drawing.Color.MintCream;
+            this.btnRegistro.FlatAppearance.BorderSize = 0;
+            this.btnRegistro.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnRegistro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistro.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistro.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRegistro.Image = global::CasetaDeVigilancia.Properties.Resources.registro;
+            this.btnRegistro.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistro.Location = new System.Drawing.Point(0, 150);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(267, 71);
+            this.btnRegistro.TabIndex = 8;
+            this.btnRegistro.Text = "Registro de\r\nResidentes";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAcceder.FlatAppearance.BorderColor = System.Drawing.Color.MintCream;
+            this.btnAcceder.FlatAppearance.BorderSize = 0;
+            this.btnAcceder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnAcceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcceder.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcceder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAcceder.Image = global::CasetaDeVigilancia.Properties.Resources.proteger;
+            this.btnAcceder.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAcceder.Location = new System.Drawing.Point(0, 100);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(267, 50);
+            this.btnAcceder.TabIndex = 7;
+            this.btnAcceder.Text = "Acceder";
+            this.btnAcceder.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(267, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(267, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel3.Controls.Add(label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(267, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(378, 450);
+            this.panel3.TabIndex = 7;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Menu";
+            this.Text = "Menú Principal";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAcceder;
+        private System.Windows.Forms.Button btnSesionGuardia;
+        private System.Windows.Forms.Button btnHistorial;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasetaDeVigilancia
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            cargarImagBotones();
+        }
+        /**
+         * Método para cargar las imágenes de los botones de la interfaz gráfica.
+         */
+        private void cargarImagBotones()
+        {
+            Image original = Properties.Resources.proteger;
+            Image redimensionada = new Bitmap(original, new Size(32, 32));
+            btnAcceder.Image = redimensionada;
+            btnAcceder.ImageAlign = ContentAlignment.MiddleLeft;
+
+            original = Properties.Resources.registro;
+            redimensionada = new Bitmap(original, new Size(32, 32));
+            btnRegistro.Image = redimensionada;
+            btnRegistro.ImageAlign = ContentAlignment.MiddleLeft;
+
+            original = Properties.Resources.historial_de_pedidos;
+            redimensionada = new Bitmap(original, new Size(32, 32));
+            btnHistorial.Image = redimensionada;
+            btnHistorial.ImageAlign = ContentAlignment.MiddleLeft;
+
+            original = Properties.Resources.iniciar_sesion;
+            redimensionada = new Bitmap(original, new Size(32, 32));
+            btnSesionGuardia.Image = redimensionada;
+            btnSesionGuardia.ImageAlign = ContentAlignment.MiddleLeft;
+        }
+    }
+}
